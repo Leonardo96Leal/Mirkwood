f1cf39e7bfa37c8ad09f458b30db3c0c54947483
UserPL022Pc12 <UserPL022Pc12@SLFRPL022PC12.ensino.atec.local>
2016-10-21 15:25:46 +0100

    ex 21/10/16

--- src/main/java/gui/Map.java
+++ src/main/java/gui/Map.java
@@ -20,149 +20,173 @@ import script.Hero;
 
 public class Map extends Panel {
 
-	public static final int COLUMNS = 50;
-	public static final int LINES = 16;
-	
-	public static final int TREECOUNT = 400;
-	public static final int BRANCHESCOUNT = 250;
-	
-	Random mRand;
-	
-	int[] playerpos = new int[]{2, 2};
-	
-	int[] waterpos = new int[LINES];
-	Tree[] treespos = new Tree[TREECOUNT];
-	Tree[] branchespos = new Tree[BRANCHESCOUNT];
-	RGB bkgColor = new TextColor.RGB(165, 127, 61);
-	
-	Characters _chars;
+    public static final int COLUMNS = 50;
+    public static final int LINES = 16;
 
-	EmptySpace land;
+    public static final int TREECOUNT = 400;
+    public static final int BRANCHESCOUNT = 250;
 
-	public Map(Characters chars) {
-		super();
+    Random mRand, ponte;
 
-		_chars = chars;
-		getBasePane();
-		
-		mRand = new Random();
-		
-		generateWater();
-		generateTrees();
+    int[] playerpos = new int[]{2, 2};
 
-		land = new EmptySpace(new TextColor.RGB(165, 127, 61)) {
-			protected ComponentRenderer<EmptySpace> createDefaultRenderer() {
-				return new ComponentRenderer<EmptySpace>() {
-					public TerminalSize getPreferredSize(EmptySpace component) {
-						return new TerminalSize(Map.COLUMNS, Map.LINES);
-					}
+    int[] waterpos = new int[LINES];
+    int[] waterpos1 = new int[COLUMNS];
+    Tree[] treespos = new Tree[TREECOUNT];
+    Tree[] branchespos = new Tree[BRANCHESCOUNT];
+    RGB bkgColor = new TextColor.RGB(165, 127, 61);
+    int cont = 0;
+    Characters _chars;
 
-					public void drawComponent(TextGUIGraphics graphics, EmptySpace component) {
-						/*
+    EmptySpace land;
+
+    public Map(Characters chars) {
+        super();
+
+        _chars = chars;
+        getBasePane();
+
+        mRand = new Random();
+
+        generateWater();
+        generateTrees();
+
+        land = new EmptySpace(new TextColor.RGB(165, 127, 61)) {
+            protected ComponentRenderer<EmptySpace> createDefaultRenderer() {
+                return new ComponentRenderer<EmptySpace>() {
+                    public TerminalSize getPreferredSize(EmptySpace component) {
+                        return new TerminalSize(Map.COLUMNS, Map.LINES);
+                    }
+
+                    public void drawComponent(TextGUIGraphics graphics, EmptySpace component) {
+                        /*
 						 * Fill background
-						 */
-						graphics.setBackgroundColor(bkgColor);
-						graphics.setModifiers(EnumSet.of(SGR.BOLD));
-						graphics.fill(' ');
-						
-						/*
+                         */
+                        graphics.setBackgroundColor(bkgColor);
+                        graphics.setModifiers(EnumSet.of(SGR.BOLD));
+                        graphics.fill(' ');
+
+                        /*
 						 * Creates the trees and branches
-						 */
-						for (Tree t : treespos) {
-							graphics.setForegroundColor(t.getColor());
-							graphics.putString(t.getmPosition().getColumn(), t.getmPosition().getRow(), String.valueOf(t.getmTree()));
-						}
-						for (Tree t : branchespos) {
-							graphics.setForegroundColor(t.getColor());
-							graphics.putString(t.getmPosition().getColumn(), t.getmPosition().getRow(), String.valueOf(t.getmTree()));
-						}
-						
-						/*
+                         */
+                        for (Tree t : treespos) {
+                            graphics.setForegroundColor(t.getColor());
+                            graphics.putString(t.getmPosition().getColumn(), t.getmPosition().getRow(), String.valueOf(t.getmTree()));
+                        }
+                        for (Tree t : branchespos) {
+                            graphics.setForegroundColor(t.getColor());
+                            graphics.putString(t.getmPosition().getColumn(), t.getmPosition().getRow(), String.valueOf(t.getmTree()));
+                        }
+
+                        /*
 						 * Creates the river
-						 */
-						graphics.setForegroundColor(new TextColor.RGB(30, 150, 200));
-						for (int i = 0; i < waterpos.length; i++) {
-							graphics.setBackgroundColor(new TextColor.RGB(30, 150, 100));
-							graphics.putString(waterpos[i], i, String.valueOf(SymbolsMirk.WATER[2]));
-							graphics.setBackgroundColor(bkgColor);
-							graphics.putString(waterpos[i]-1, i, String.valueOf(SymbolsMirk.WATER[1]));
-							graphics.putString(waterpos[i]+1, i, String.valueOf(SymbolsMirk.WATER[1]));
-							graphics.putString(waterpos[i]-2, i, String.valueOf(SymbolsMirk.WATER[0]));
-							graphics.putString(waterpos[i]+2, i, String.valueOf(SymbolsMirk.WATER[0]));
-						}
-						
-						/*
+                         */
+                        graphics.setForegroundColor(new TextColor.RGB(30, 150, 200));
+                        final int pon = mRand.nextInt(LINES);
+                        for (int i = 0; i < waterpos.length; i++) {
+                            graphics.setBackgroundColor(new TextColor.RGB(30, 150, 100));
+                            graphics.putString(waterpos[i], i, String.valueOf(SymbolsMirk.WATER[2]));
+                            graphics.setBackgroundColor(bkgColor);
+                            graphics.putString(waterpos[i] - 1, i, String.valueOf(SymbolsMirk.WATER[1]));
+                            graphics.putString(waterpos[i] + 1, i, String.valueOf(SymbolsMirk.WATER[1]));
+                            graphics.putString(waterpos[i] - 2, i, String.valueOf(SymbolsMirk.WATER[0]));
+                            graphics.putString(waterpos[i] + 2, i, String.valueOf(SymbolsMirk.WATER[0]));
+                            while(cont==0){
+                                //graphics.setBackgroundColor(new TextColor.RGB(0, 0, 0));
+                                graphics.setForegroundColor(TextColor.ANSI.BLACK);
+                                graphics.putString(waterpos[pon], pon, String.valueOf(SymbolsMirk.HERO));
+                                graphics.setForegroundColor(new TextColor.RGB(30, 150, 200));
+                                cont++;
+                            }
+                        }
+                        graphics.putString(15, 5, String.valueOf(SymbolsMirk.WATER[2]));
+                        /* for (int i = 0; i < waterpos1.length; i++) {
+                            graphics.setBackgroundColor(new TextColor.RGB(30, 150, 100));
+                            graphics.putString(i, waterpos1[i], String.valueOf(SymbolsMirk.WATER[2]));
+                            graphics.setBackgroundColor(bkgColor);
+                            graphics.putString(i, waterpos1[i] - 1, String.valueOf(SymbolsMirk.WATER[1]));
+                            graphics.putString(i, waterpos1[i] + 1, String.valueOf(SymbolsMirk.WATER[1]));
+                            graphics.putString(i,waterpos1[i] - 2, String.valueOf(SymbolsMirk.WATER[0]));
+                            graphics.putString(i,waterpos1[i] + 2, String.valueOf(SymbolsMirk.WATER[0]));
+                        }*/
+
+ /*
 						 * Draw characters
-						 */
-						Hero h = _chars.getHero();
-						graphics.setBackgroundColor(h.get_bkgColor());
-						graphics.setForegroundColor(h.get_foregroundColor());
-						graphics.setCharacter(h.get_position(), h.get_face());
+                         */
+                        Hero h = _chars.getHero();
+                        graphics.setBackgroundColor(h.get_bkgColor());
+                        graphics.setForegroundColor(h.get_foregroundColor());
+                        graphics.setCharacter(h.get_position(), h.get_face());
 
-						graphics.setModifiers(EnumSet.of(SGR.BLINK));
-						Foe f = _chars.getFoe();
-						graphics.setBackgroundColor(f.get_bkgColor());
-						graphics.setForegroundColor(f.get_foregroundColor());
-						graphics.setCharacter(f.get_position(), f.get_face());
-					}
-				};
-			}
-		};
+                        graphics.setModifiers(EnumSet.of(SGR.BLINK));
+                        Foe f = _chars.getFoe();
+                        graphics.setBackgroundColor(f.get_bkgColor());
+                        graphics.setForegroundColor(f.get_foregroundColor());
+                        graphics.setCharacter(f.get_position(), f.get_face());
+                    }
+                };
+            }
+        };
 
-		addComponent(land);
+        addComponent(land);
 
-	}
-	
-	public void generateWater() {
-		int col = mRand.nextInt(COLUMNS);
-		for (int i = 0; i < LINES; i++) {
-			waterpos[i] = col + (mRand.nextInt(2) - 1);
-		}
-	}
-	
-	public void generateTrees() {
-		for (int i=0; i < TREECOUNT; i++)
-			treespos[i] = Tree.factoryRandomTree(COLUMNS, LINES);
+    }
 
-		for (int i=0; i < BRANCHESCOUNT; i++)
-			branchespos[i] = Tree.factoryRandomBranch(COLUMNS, LINES);
-	}
+    public void generateWater() {
+        int col = mRand.nextInt(COLUMNS);
+        for (int i = 0; i < LINES; i++) {
+            waterpos[i] = col + (mRand.nextInt(2) - 1);
+        }
+        int col1 = mRand.nextInt(LINES);
+        for (int i = 0; i < COLUMNS; i++) {
+            waterpos1[i] = col1 + (mRand.nextInt(2) - 1);
+        }
+    }
 
-	public void refreshLand() {
-		land.invalidate();
-	}
-	
-	public void updatePlayer(KeyStroke keyStroke) {
-		TerminalPosition ppos = _chars.getHero().get_position();
-		Hero player = _chars.getHero();
-		switch (keyStroke.getCharacter()) {
-		case 'w':
-			player.set_position(new TerminalPosition(ppos.getColumn(), ppos.getRow() - 1));
-			break;
-		case 's':
-			player.set_position(new TerminalPosition(ppos.getColumn(), ppos.getRow()+1));
-			break;
-		case 'a':
-			player.set_position(new TerminalPosition(ppos.getColumn()-1, ppos.getRow()));
-			break;
-		case 'd':
-			player.set_position(new TerminalPosition(ppos.getColumn()+1, ppos.getRow()));
-			break;
-		default:
-			System.out.println(keyStroke.getCharacter().toString());
-			break;
-		}
-		
-		refreshLand();
-	}
+    public void generateTrees() {
+        for (int i = 0; i < TREECOUNT; i++) {
+            treespos[i] = Tree.factoryRandomTree(COLUMNS, LINES);
+        }
 
-	/*
+        for (int i = 0; i < BRANCHESCOUNT; i++) {
+            branchespos[i] = Tree.factoryRandomBranch(COLUMNS, LINES);
+        }
+    }
+
+    public void refreshLand() {
+        land.invalidate();
+    }
+
+    public void updatePlayer(KeyStroke keyStroke) {
+        TerminalPosition ppos = _chars.getHero().get_position();
+        Hero player = _chars.getHero();
+        switch (keyStroke.getCharacter()) {
+            case 'w':
+                player.set_position(new TerminalPosition(ppos.getColumn(), ppos.getRow() - 1));
+                break;
+            case 's':
+                player.set_position(new TerminalPosition(ppos.getColumn(), ppos.getRow() + 1));
+                break;
+            case 'a':
+                player.set_position(new TerminalPosition(ppos.getColumn() - 1, ppos.getRow()));
+                break;
+            case 'd':
+                player.set_position(new TerminalPosition(ppos.getColumn() + 1, ppos.getRow()));
+                break;
+            default:
+                System.out.println(keyStroke.getCharacter().toString());
+                break;
+        }
+
+        refreshLand();
+    }
+
+    /*
 	 * @Override protected void onAfterDrawing(TextGUIGraphics graphics) { //
 	 * TODO Auto-generated method stub super.onAfterDrawing(graphics);
 	 * graphics.setForegroundColor(TextColor.ANSI.CYAN);
 	 * graphics.setBackgroundColor(TextColor.ANSI.BLUE);
 	 * graphics.setModifiers(EnumSet.of(SGR.BOLD)); graphics.fill(' ');
 	 * graphics.putString(3, 0, "Text GUI in 100% Java"); }
-	 */
-
+     */
 }
---
+++ src/main/java/script/Character.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package script;
+
+/**
+ *
+ * @author UserPL022Pc12
+ */
+abstract public class Character {
+    int power,attack,defense;
+    
+    
+}
