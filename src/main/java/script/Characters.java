c9bdda45ce7ba966b869d27323168bd1f366bc94
UserPL022Pc11 <UserPL022Pc11@SLFRPL022PC11.ensino.atec.local>
2016-11-24 13:56:22 +0000

    mudancas

--- src/main/java/script/Characters.java
+++ src/main/java/script/Characters.java
@@ -12,9 +12,10 @@ public class Characters {
 	public Characters() {
 		super();
 		
-		hero = new Hero("Sir Guinetti", new TerminalPosition(2, Map.LINES-2));
+		hero = new Hero("Sir Guinetti", new TerminalPosition(2, 2));
 		foe = new Foe("Sauron", new TerminalPosition(Map.COLUMNS-2, 2));
-	}
+	
+        }
 
 	public Hero getHero() {
 		return hero;
